2f3e1d368881936d02161012bda2bf7c2e603470
Shubh Chaudhary <106395117+Lordeyesgaming@users.noreply.github.com>
2024-05-31 15:20:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+    function showSection(sectionId) {
+        var sections = document.querySelectorAll('.content section');
+        sections.forEach(function(section) {
+            section.style.display = 'none';
+        });
+        document.getElementById(sectionId).style.display = 'block';
+    }
+</script>
